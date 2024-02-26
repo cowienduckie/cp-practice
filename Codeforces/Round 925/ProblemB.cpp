aed36745e38b140e6138af01c50fefa2859e967d
Minh Tran <minhtran.2404@outlook.com>
2024-02-26 12:55:28 +0700

    feat: add Codeforces round 925

---
+++ Codeforces/Round 925/ProblemB.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int                 long long int
+#define double              long double
+#define INF                 1e18
+
+#define pb                  push_back
+#define endl                "\n"
+
+#define pii                 pair <int,int>
+
+#define vi                  vector <int>
+#define vpi                 vector <pii>
+
+#define si                  set <int>
+#define spi                 set <pii>
+
+#define umpi                unordered_map <int,int>
+#define mpi                 map <int,int>
+#define mpp                 map <pii, int>
+
+#define pq_max              priority_queue <int>
+#define pq_min              priority_queue <int, vi, greater <int>>
+
+#define print(v)            for(auto x : v) cout << x << " "; cout << endl
+#define print_pair(v)       for(auto x : v) cout << x.f << " " << x.s << endl
+#define print_range(v,x,y)  for(int i = x; i < y; i++) cout<< v[i]<< " "; cout << endl
+
+#define rep(i,n)            for (int i = 0; i < n; ++i)
+#define REP(i,k,n)          for (int i = k; i <= n; ++i) 
+#define REPR(i,k,n)         for (int i = k; i >= n; --i)
+
+#define yn(x)               cout << (x ? "YES" :"NO") << endl
+
+#define bug(...)            __f (#__VA_ARGS__, __VA_ARGS__)
+
+#define all(v)              v.begin(), v.end()
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { 
+    cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+const int MOD = 1000000000 + 7;
+
+int GCD(int a, int b) {
+    return b == 0 ? a : GCD(b, a % b);
+}
+
+int power(int x, int y) {
+    return y ? ((y % 2 ? x : 1ll) * power((x * x) % MOD, y / 2) % MOD) % MOD : 1ll;
+}
+
+int fast_ceil(int x, int y) {
+    return 1 + ((x - 1) / y);
+}
+
+void initialize() {
+    
+}
+
+void solve() {
+    int n, tank = 0, avg = 0; cin >> n;
+    int arr[N];
+
+    rep(i, n)
+    {
+        int a; cin >> a; 
+        
+        arr[i] = a;
+        avg += a;
+    }
+
+    avg /= n;
+
+    rep(i, n)
+    {
+        if (arr[i] >= avg)
+        {
+            tank += arr[i] - avg;
+            continue;
+        }
+
+        if (arr[i] + tank < avg)
+        {
+            cout << "NO" << endl;
+            return;
+        }
+
+        tank -= avg - arr[i];
+    }
+
+    cout << "YES" << endl;
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    //clock_t startTime = clock();
+
+    int t = 1;
+    cin >> t;
+
+    initialize();
+
+    while(t--){
+        solve();
+    }
+
+    //cerr << "Run Time: " << (double)(clock() - startTime) / CLOCKS_PER_SEC << "s" << endl;
+}
---
+++ Codeforces/Round 925/ProblemD.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int                 long long int
+#define double              long double
+#define INF                 1e18
+
+#define pb                  push_back
+#define endl                "\n"
+
+#define pii                 pair <int,int>
+
+#define vi                  vector <int>
+#define vpi                 vector <pii>
+
+#define si                  set <int>
+#define spi                 set <pii>
+
+#define umpi                unordered_map <int,int>
+#define mpi                 map <int,int>
+#define mpp                 map <pii, int>
+
+#define pq_max              priority_queue <int>
+#define pq_min              priority_queue <int, vi, greater <int>>
+
+#define print(v)            for(auto x : v) cout << x << " "; cout << endl
+#define print_pair(v)       for(auto x : v) cout << x.f << " " << x.s << endl
+#define print_range(v,x,y)  for(int i = x; i < y; i++) cout<< v[i]<< " "; cout << endl
+
+#define rep(i,n)            for (int i = 0; i < n; ++i)
+#define REP(i,k,n)          for (int i = k; i <= n; ++i) 
+#define REPR(i,k,n)         for (int i = k; i >= n; --i)
+
+#define yn(x)               cout << (x ? "YES" :"NO") << endl
+
+#define bug(...)            __f (#__VA_ARGS__, __VA_ARGS__)
+
+#define all(v)              v.begin(), v.end()
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { 
+    cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 2 * 1e5;
+const int MOD = 1000000000 + 7;
+
+int GCD(int a, int b) {
+    return b == 0 ? a : GCD(b, a % b);
+}
+
+int power(int x, int y) {
+    return y ? ((y % 2 ? x : 1ll) * power((x * x) % MOD, y / 2) % MOD) % MOD : 1ll;
+}
+
+int fast_ceil(int x, int y) {
+    return 1 + ((x - 1) / y);
+}
+
+void initialize() {
+    
+}
+
+void solve() {
+    int n, x, y, counter = 0; cin >> n >> x >> y;
+    mpp mod;
+
+    rep(i, n)
+    {
+        int a; cin >> a;
+
+        counter += mod[{ (x - (a % x)) % x, a % y }];
+
+        ++mod[{ a % x, a % y }];
+    }
+
+    cout << counter << endl;
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    //clock_t startTime = clock();
+
+    int t = 1;
+    cin >> t;
+
+    initialize();
+
+    while(t--){
+        solve();
+    }
+
+    //cerr << "Run Time: " << (double)(clock() - startTime) / CLOCKS_PER_SEC << "s" << endl;
+}
